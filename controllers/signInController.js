86e373eac5b6db2c52bf649920f3f7a83fd1bacf
minjaekim <minjae2246@gmail.com>
2021-10-13 00:06:13 +0900

    Add: 로그인 KeyError에러처리 추가

--- controllers/signInController.js
+++ controllers/signInController.js
@@ -4,17 +4,23 @@ import apiError from '../errors/apiError';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const signInUser = async (req, res, next) => {
+const signInUser = async (req, res) => {
   try {
-    const { email, password } = req.body;
-    const accessToken = await signInService.signInUser(email, password);
+    const userInputValue = Object.keys(req.body);
+    const totalNumberOfUserInput = 2;
 
+    if (userInputValue.length !== totalNumberOfUserInput) {
+      next(apiError.keyError('키가 비어있습니다'));
+    }
+
+    const { email, password } = req.body;
+
+    const accessToken = await signInService.signInUser(email, password);
     const decodedToken = jwtToken.verify(accessToken);
     res.cookie('jwt', accessToken, {
       expiresIn: process.env.JWT_TTL,
       httpOnly: true,
     });
-    next();
 
     return res.status(200).json({
       msg: 'SUCCESS_SIGNIN',
@@ -23,7 +29,7 @@ const signInUser = async (req, res, next) => {
   } catch (error) {
     // next(apiError.badRequest('잘못된 요청임당'));
     const { statusCode, message } = error;
-    return res.status(404).json({
+    return res.status(statusCode).json({
       msg: message,
     });
   }
