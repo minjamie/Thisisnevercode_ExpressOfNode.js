e0de538a84c7f03cd1753114b0189e1582552114
minjaekim <minjae2246@gmail.com>
2021-10-12 22:49:56 +0900

    Add: gitigonre에migrations 파일 추가

--- controllers/signUpController.js
+++ controllers/signUpController.js
@@ -4,28 +4,22 @@ const createUser = async (req, res) => {
   try {
     const userInfo = req.body;
 
-    const userData = Object.entries(req.body);
-    for (let userInputData of userData) {
-      console.log(userInputData.length);
-      if (userInputData.length < 0) {
-        res.status(404).json({
-          msg: 'KEY ERROR',
-        });
-      } else if (userInputData === 1) {
-        const error = new Error('EMPTY_VALUE');
-        error.statusCode = 404;
-        error.message = '값이 비어있습니다.';
-        throw error;
-      } else {
-        await signUpService.createUser(userInfo);
-        res.status(201).json({
-          msg: 'SUCCESS_SIGNUP',
-        });
-      }
+    const userInputValue = Object.keys(req.body);
+    const totalNumberOfUserInput = 4;
+    if (userInputValue.length !== totalNumberOfUserInput) {
+      const error = new Error('Key_Error');
+      error.statusCode = 404;
+      error.message = '키가 비어있습니다.';
+      throw error;
     }
+
+    await signUpService.createUser(userInfo);
+    res.status(201).json({
+      msg: 'SUCCESS_SIGNUP',
+    });
   } catch (error) {
     const { statusCode, message } = error;
-    res.status(statusCode).json({
+    res.status(400).json({
       msg: message,
     });
   }
