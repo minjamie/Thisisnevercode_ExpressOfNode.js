9417f4c890c2c7a7884d15ac627b9f8753ab2842
minjaekim <minjae2246@gmail.com>
2021-10-13 00:04:00 +0900

    Modified: app.js에 에러핸들러 수정

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 import express from 'express';
 import routes from './routes';
-import dotenv from 'dotenv';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
+import apiError, { notFoundError } from './errors/apiError';
+import errorHandler from './errors/errorHandler';
 
 const app = express();
-
-dotenv.config();
 
 app.use(morgan('dev'));
 app.use(cookieParser('secret'));
@@ -14,15 +13,15 @@ app.use(express.json());
 
 app.use(routes);
 
-// app.use((req, res, next) => {
-//   res.status(404).send('Not Found');
-//   next();
-// });
+app.use(errorHandler);
+
+app.use((req, res, next) => {
+  next(apiError.notFoundError('Not Found'));
+});
 
 app.use((err, req, res, next) => {
-  const { statusCode, message } = err;
-  console.error(err);
-  res.status(statusCode || 500).send(message);
+  const { code, msg } = err;
+  res.status(code).json({ msg });
 });
 
 export default app;
