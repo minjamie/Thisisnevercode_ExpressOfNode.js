b12235e22cf65aecef009384c0c0d83c92c999b5
Jilpung <bodkl5500@gmail.com>
2021-10-12 10:32:21 +0900

    Add: first commit

--- models/listDao.js
+++ models/listDao.js
@@ -3,9 +3,27 @@ import prisma from '../prisma';
 const getList = async () => {
   return await prisma.$queryRaw`
     SELECT 
-      c.id, c.category_name
+      i.id,
+      i.main_image_url,
+      i.sub_image_url,
+      i.detail_image_url,
+      p.english_name,
+      p.price
     FROM 
-      categories c
+      images i
+    LEFT JOIN
+      products p
+    ON
+      i.product_id = p.id
+    WHERE
+      i.id > 0
+    order by p.id asc
+    limit 5;
+  `;
+};
+
+const getProductImages = async (id) => {
+  return await prisma.$queryRaw`
   `;
 };
 
