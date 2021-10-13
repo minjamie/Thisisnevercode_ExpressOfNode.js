c6cd48b81bc9fa1b1321c5413797af1a469f4016
Jilpung <bodkl5500@gmail.com>
2021-10-13 15:22:48 +0900

    Add: try, catch 추가

--- controllers/listController.js
+++ controllers/listController.js
@@ -1,14 +1,22 @@
 import { listService } from '../services';
 
 const getList = async (req, res) => {
-  const id = req.params.id;
-  const product = await listService.getList(id);
-  res.status(200).json({
-    LIST_DATA: {
-      category: 'SHOES',
-      product,
-    },
-  });
+  try {
+    const id = req.params.id;
+    const product = await listService.getList(id);
+    res.status(200).json({
+      LIST_DATA: {
+        category: 'SHOES',
+        product,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    const { statusCode, message } = err;
+    res.status(statusCode || 500).json({
+      msg: message,
+    });
+  }
 };
 
 export default { getList };
